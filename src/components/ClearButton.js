09059c3a2c7d435cc5ad868fa69f93844b3269da
Sofia_Gutierrez <sofiagutierrez1845@gmail.com>
2023-03-14 10:15:28 -0300

    agregando estilos generales

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../assets/css/ClearButton.css"
 
 const ClearButton = ({ children, handleClear }) => (
-    <div className="clear-button" onClick={handleClear}>
+    <button className="clear-button" onClick={handleClear}>
         {children}
-    </div>
+    </button>
 );
 
 export default ClearButton;
